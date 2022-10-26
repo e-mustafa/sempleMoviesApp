72e4c0999798faf3a875ee2e01e4ea94c87538ea
Mustafa Ahmed <105460460+My6000@users.noreply.github.com>
2022-10-26 23:12:25 +0200

    page404 img edit

--- src/Components/Page404.jsx
+++ src/Components/Page404.jsx
@@ -14,7 +14,7 @@ const Page404 = () => {
   return (
       <div>
          <Stack  display='flex' justifyContent='center' alignItems='center'  p={2} >
-            <img src={img404} alt="page not found" style={{filter: 'grayscale(100%)'}} />
+            <img src={img404} alt="page not found" style={{filter: 'grayscale(100%)', width:'100%'}} />
             <Button variant="contained" color='warning' onClick={() => navigate('/1')} >الرجوع للصفحة الرئيسية</Button>
          </Stack>
       </div>
