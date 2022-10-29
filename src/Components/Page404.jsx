34a175bbc1599bdeca15627bf88eb7fe31206a7a
Mustafa Ahmed <105460460+My6000@users.noreply.github.com>
2022-10-29 02:49:36 +0200

    _redirects

--- src/Components/Page404.jsx
+++ src/Components/Page404.jsx
@@ -11,14 +11,14 @@ const Page404 = () => {
    const navigate = useNavigate()
 
 
-  return (
+   return (
       <div>
          <Stack  display='flex' justifyContent='center' alignItems='center'  p={2} >
             <img src={img404} alt="page not found" style={{filter: 'grayscale(100%)', width:'100%'}} />
             <Button variant="contained" color='warning' onClick={() => navigate('/1')} >الرجوع للصفحة الرئيسية</Button>
          </Stack>
       </div>
-  )
+   )
 }
 
 
